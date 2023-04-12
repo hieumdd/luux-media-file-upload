97cc881549948ab782c3fa83a458baa98c5a4551
HM <hieumdd@gmail.com>
2023-04-12 10:42:41 +0700

    fix: Fix typings

--- src/bigquery/bigquery.service.ts
+++ src/bigquery/bigquery.service.ts
@@ -2,13 +2,8 @@ import { Readable } from 'stream';
 import { pipeline } from 'stream/promises';
 
 import ndjson from 'ndjson';
-import { BigQuery } from '@google-cloud/bigquery';
+import { BigQuery, TableSchema } from '@google-cloud/bigquery';
 import dayjs from 'dayjs';
-
-type AddBatchedAtOptions = {
-    rows: Record<string, any>[];
-    schema: Record<string, any>[];
-};
 
 type LoadOptions = {
     table: string;
@@ -19,22 +14,22 @@ const client = new BigQuery();
 
 const DATASET = 'FileUpload';
 
-const addBatchedAt = ({ rows, schema }: AddBatchedAtOptions) => [
-    rows.map((row) => ({ ...row, _batched_at: dayjs().toISOString() })),
-    [...schema, { name: '_batched_at', type: 'TIMESTAMP' }],
-];
-
 export const load = async (rows: Record<string, any>[], options: LoadOptions) => {
-    const [_rows, fields] = addBatchedAt({ rows, schema: options.schema });
+    const tableWriteStream = client
+        .dataset(DATASET)
+        .table(`p_${options.table}`)
+        .createWriteStream({
+            schema: {
+                fields: [...options.schema, { name: '_batched_at', type: 'TIMESTAMP' }],
+            } as TableSchema,
+            sourceFormat: 'NEWLINE_DELIMITED_JSON',
+            createDisposition: 'CREATE_IF_NEEDED',
+            writeDisposition: 'WRITE_APPEND',
+        });
 
-    const tableWriteStream = client.dataset(DATASET).table(`p_${options.table}`).createWriteStream({
-        schema: { fields },
-        sourceFormat: 'NEWLINE_DELIMITED_JSON',
-        createDisposition: 'CREATE_IF_NEEDED',
-        writeDisposition: 'WRITE_APPEND',
-    });
-
-    return pipeline(Readable.from(_rows), ndjson.stringify(), tableWriteStream).then(
-        () => rows.length,
-    );
+    return pipeline(
+        Readable.from(rows.map((row) => ({ ...row, _batched_at: dayjs().toISOString() }))),
+        ndjson.stringify(),
+        tableWriteStream,
+    ).then(() => rows.length);
 };
