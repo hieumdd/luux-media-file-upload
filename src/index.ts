11f6d43e6ecca61e1e63c207077a208aaf998660
HM <hieumdd@gmail.com>
2023-04-28 22:58:02 +0700

    feat: Update schema

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { parse } from 'csv-parse/sync';
 import express from 'express';
 import dayjs from 'dayjs';
 import customParseFormat from 'dayjs/plugin/customParseFormat';
+import utc from 'dayjs/plugin/utc';
 import { http } from '@google-cloud/functions-framework';
 import Joi from 'joi';
 import standardFormidable from 'formidable';
@@ -11,6 +12,7 @@ const formidable: typeof standardFormidable = require('formidable-serverless');
 import { load } from './bigquery/bigquery.service';
 
 dayjs.extend(customParseFormat);
+dayjs.extend(utc);
 
 const app = express();
 
@@ -34,78 +36,44 @@ app.post('/upload', (req, res) => {
     })
         .then((data) => {
             const timestampSchema = Joi.string()
+                .empty('(No value)')
                 .empty('')
                 .custom((value: string) => {
-                    const dt = dayjs(value, 'DD/MM/YYYY HH:mm');
+                    const dt = dayjs.utc(value, 'DD/MM/YYYY HH:mm');
                     return dt.isValid() ? dt.format('YYYY-MM-DDTHH:mm:ss') : null;
                 });
 
+            const stringSchema = Joi.string().empty('(No value)').empty('').default(null);
+
             const schema = Joi.object({
-                associated_comapny_ids: Joi.string().empty('').default(null),
-                associated_company: Joi.string().empty('').default(null),
-                campaign: Joi.string().empty('').default(null),
-                contact_owner: Joi.string().empty('').default(null),
                 create_date: timestampSchema,
-                email: Joi.string().empty('').default(null),
-                first_name: Joi.string().empty('').default(null),
-                first_page_seen: Joi.string().empty('').default(null),
-                gclid: Joi.string().empty('').default(null),
-                ip_timezone: Joi.string().empty('').default(null),
-                last_activity_date: timestampSchema,
-                last_name: Joi.string().empty('').default(null),
-                lead_status: Joi.string().empty('').default(null),
-                location: Joi.string().empty('').default(null),
-                original_source: Joi.string().empty('').default(null),
-                phone_number: Joi.string().empty('').default(null),
-                primary_associated_company_id: Joi.string().empty('').default(null),
-                record_id: Joi.number(),
-                timezone: Joi.string().empty('').default(null),
-                utm_campaign: Joi.string().empty('').default(null),
-                utm_content: Joi.string().empty('').default(null),
-                utm_source: Joi.string().empty('').default(null),
+                contact_id: Joi.number().unsafe(),
+                gclid: stringSchema,
+                ip_timezone: stringSchema,
+                campaign: stringSchema,
+                lead_status: stringSchema,
+                adgroup_name: stringSchema,
+                utm_campaign: stringSchema,
+                utm_content: stringSchema,
+                utm_source: stringSchema,
             })
-                .rename('Record ID', 'record_id')
-                .rename('First Name', 'first_name')
-                .rename('Last Name', 'last_name')
-                .rename('Email', 'email')
-                .rename('Phone Number', 'phone_number')
-                .rename('Contact owner', 'contact_owner')
-                .rename('Primary Associated Company ID', 'primary_associated_company_id')
-                .rename('Last Activity Date', 'last_activity_date')
-                .rename('Lead Status', 'lead_status')
-                .rename('Create Date', 'create_date')
                 .rename('Campaign', 'campaign')
-                .rename('Location', 'location')
-                .rename('Time Zone', 'timezone')
+                .rename('Create Date', 'create_date')
                 .rename('IP Timezone', 'ip_timezone')
-                .rename('Associated Company', 'associated_company')
-                .rename('Original Source', 'original_source')
-                .rename('First Page Seen', 'first_page_seen')
-                .rename('Associated Company IDs', 'associated_comapny_ids');
+                .rename('Lead Status', 'lead_status')
+                .rename('Contact ID', 'contact_id');
 
             return data.map((row) => Joi.attempt(row, schema, { stripUnknown: true }));
         })
         .then((data) => {
             const schema = [
-                { name: 'associated_comapny_ids', type: 'STRING' },
-                { name: 'associated_company', type: 'STRING' },
-                { name: 'campaign', type: 'STRING' },
-                { name: 'contact_owner', type: 'STRING' },
                 { name: 'create_date', type: 'TIMESTAMP' },
-                { name: 'email', type: 'STRING' },
-                { name: 'first_name', type: 'STRING' },
-                { name: 'first_page_seen', type: 'STRING' },
+                { name: 'contact_id', type: 'INT64' },
                 { name: 'gclid', type: 'STRING' },
                 { name: 'ip_timezone', type: 'STRING' },
-                { name: 'last_activity_date', type: 'TIMESTAMP' },
-                { name: 'last_name', type: 'STRING' },
+                { name: 'campaign', type: 'STRING' },
                 { name: 'lead_status', type: 'STRING' },
-                { name: 'location', type: 'STRING' },
-                { name: 'original_source', type: 'STRING' },
-                { name: 'phone_number', type: 'STRING' },
-                { name: 'primary_associated_company_id', type: 'STRING' },
-                { name: 'record_id', type: 'INT64' },
-                { name: 'timezone', type: 'STRING' },
+                { name: 'adgroup_name', type: 'STRING' },
                 { name: 'utm_campaign', type: 'STRING' },
                 { name: 'utm_content', type: 'STRING' },
                 { name: 'utm_source', type: 'STRING' },
